f9930fe4ac0cdfcb9ad83d2f43ecdebd72e1e1a9
Jonathan DEKHTIAR <contact@jonathandekhtiar.eu>
2014-04-19 21:52:12 +0200

    Template Config Atualisé

--- config.inc.dist.php
+++ config.inc.dist.php
@@ -7,5 +7,8 @@ $_CONFIG['sql_user'] = "billetterie";
 $_CONFIG['sql_pass'] = "billetterie";
 
 // Chemin vers le serveur CAS (avec le / final)
-$_CONFIG['cas_url'] = "";
-$_CONFIG['service'] = "http://stagestx.jonathandekhtiar.eu/";
+$_CONFIG['cas_url'] = "https://cas.utc.fr/cas/";
+$_CONFIG['service'] = "http://stagestx.jonathandekhtiar.eu/inc/connect.php";
+$_CONFIG['home'] = "http://stagestx.jonathandekhtiar.eu/";
+
+?>

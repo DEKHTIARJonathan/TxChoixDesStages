46c5448f925a5f54788af6b807e79a1c05c9e07e
Jonathan DEKHTIAR <contact@jonathandekhtiar.eu>
2014-06-20 02:30:51 +0200

    checkExtensions updated

--- inc/checkExtensions.php
+++ inc/checkExtensions.php
@@ -1,26 +1,22 @@
 <?php
 	
-	if (!extension_loaded('gd') || !extension_loaded('xml') || (!class_exists('ZipArchive') && !extension_loaded('zip')))
-	{
-		if(class_exists('ZipArchive') || extension_loaded('zip'))
-			echo "zip installed & running";
-		else
-			echo "L'extension 'zip' et la classe ZipArchive (php récent) n'est pas chargée sur le serveur => configuration PHP";
+	if(class_exists('ZipArchive') || extension_loaded('zip'))
+		echo "zip installed & running";
+	else
+		echo "L'extension 'zip' et la classe ZipArchive (php récent) n'est pas chargée sur le serveur => configuration PHP";
 
-		echo "<br><br>";
+	echo "<br><br>";
 
-		if (extension_loaded('xml'))
-	        echo "xml installed & running";
-	    else
-	    	echo "L'extension 'xml' n'est pas chargée sur le serveur => configuration PHP";
+	if (extension_loaded('xml'))
+        echo "xml installed & running";
+    else
+    	echo "L'extension 'xml' n'est pas chargée sur le serveur => configuration PHP";
 
-	    echo "<br><br>";
+    echo "<br><br>";
 
-	    if (extension_loaded('gd'))
-	    	echo "gd installed & running";
-	    else 
-	    	echo "L'extension 'gd' n'est pas chargée sur le serveur => configuration PHP";
+    if (extension_loaded('gd'))
+    	echo "gd installed & running";
+    else 
+    	echo "L'extension 'gd' n'est pas chargée sur le serveur => configuration PHP";
 
-	    die();
-	}
 ?>

1cdc15fa30be707e5297ef983479fe4f6ff8f805
113216 <u79556561@false.(none)>
2015-01-03 02:21:42 +0100

    User Report

---
+++ Report/user/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+find . -name '*.out' -delete
+find . -name '*.log' -delete
+find . -name '*.aux' -delete
+find . -name '*.pdf' -delete
+find . -name '*.gz' -delete
+find . -name '*.toc' -delete
+find . -name '*.bbl' -delete
+find . -name '*.blg' -delete
+find . -name '*.dvi' -delete
+find . -name '*.bak' -delete
